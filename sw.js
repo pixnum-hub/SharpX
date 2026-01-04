d45d1560eb1a37707d12b8506a32e74d5acd4c01
Manik Roy <pixnum@gmail.com>
2026-01-04 16:59:02 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,36 +1,13 @@
-const CACHE_NAME = "sharpx-v1";
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
-];
+const CACHE = "sharpx-v2";
 
-self.addEventListener("install", event => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open(CACHE).then(c=>{
+      c.addAll(["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"]);
+    })
   );
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
-  );
-});
-
-self.addEventListener("message", event => {
-  if(event.data?.action === "skipWaiting") self.skipWaiting();
+self.addEventListener("fetch", e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
